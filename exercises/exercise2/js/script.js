7129c172b83d32084bfed27c207e6a1d0db533b5
PercyNguyen7 <70938896+PercyNguyen7@users.noreply.github.com>
2021-02-04 02:35:28 -0500

    E2: Made rock paper scissors game. Upon the player say "Go + their choice", the code will response with a random choice. If the player loses, their choice turns  red, if win choice turns green, if tie choice turns grey

--- exercises/exercise2/js/script.js
+++ exercises/exercise2/js/script.js
@@ -1,143 +1,12 @@
 "use strict";
 
-const animals =     [
-      "aardvark",
-      "alligator",
-      "alpaca",
-      "antelope",
-      "ape",
-      "armadillo",
-      "baboon",
-      "badger",
-      "bat",
-      "bear",
-      "beaver",
-      "bison",
-      "boar",
-      "buffalo",
-      "bull",
-      "camel",
-      "canary",
-      "capybara",
-      "cat",
-      "chameleon",
-      "cheetah",
-      "chimpanzee",
-      "chinchilla",
-      "chipmunk",
-      "cougar",
-      "cow",
-      "coyote",
-      "crocodile",
-      "crow",
-      "deer",
-      "dingo",
-      "dog",
-      "donkey",
-      "dromedary",
-      "elephant",
-      "elk",
-      "ewe",
-      "ferret",
-      "finch",
-      "fish",
-      "fox",
-      "frog",
-      "gazelle",
-      "gila monster",
-      "giraffe",
-      "gnu",
-      "goat",
-      "gopher",
-      "gorilla",
-      "grizzly bear",
-      "ground hog",
-      "guinea pig",
-      "hamster",
-      "hedgehog",
-      "hippopotamus",
-      "hog",
-      "horse",
-      "hyena",
-      "ibex",
-      "iguana",
-      "impala",
-      "jackal",
-      "jaguar",
-      "kangaroo",
-      "koala",
-      "lamb",
-      "lemur",
-      "leopard",
-      "lion",
-      "lizard",
-      "llama",
-      "lynx",
-      "mandrill",
-      "marmoset",
-      "mink",
-      "mole",
-      "mongoose",
-      "monkey",
-      "moose",
-      "mountain goat",
-      "mouse",
-      "mule",
-      "muskrat",
-      "mustang",
-      "mynah bird",
-      "newt",
-      "ocelot",
-      "opossum",
-      "orangutan",
-      "oryx",
-      "otter",
-      "ox",
-      "panda",
-      "panther",
-      "parakeet",
-      "parrot",
-      "pig",
-      "platypus",
-      "polar bear",
-      "porcupine",
-      "porpoise",
-      "prairie dog",
-      "puma",
-      "rabbit",
-      "raccoon",
-      "ram",
-      "rat",
-      "reindeer",
-      "reptile",
-      "rhinoceros",
-      "salamander",
-      "seal",
-      "sheep",
-      "shrew",
-      "silver fox",
-      "skunk",
-      "sloth",
-      "snake",
-      "squirrel",
-      "tapir",
-      "tiger",
-      "toad",
-      "turtle",
-      "walrus",
-      "warthog",
-      "weasel",
-      "whale",
-      "wildcat",
-      "wolf",
-      "wolverine",
-      "wombat",
-      "woodchuck",
-      "yak",
-      "zebra"
+const choices = [
+  "rock",
+  "paper",
+  "scissors",
     ];
 
-let currentAnimal = ``;
+let currentChoice = ``;
 let currentAnswer = ``;
 
 function setup() {
@@ -145,14 +14,10 @@ function setup() {
 
   if (annyang) {
     let commands = {
-      'I think it is *animal': guessAnimal
+      'Go *choice':playRandom
     };
     annyang.addCommands(commands);
     annyang.start();
-
-    textSize(32);
-    textStyle(BOLD);
-    textAlign(CENTER,CENTER);
   }
 }
 
@@ -160,7 +25,12 @@ function setup() {
 function draw() {
   background(0);
 
-  if (currentAnswer === currentAnimal){
+  if (currentChoice === currentAnswer){
+    fill(120);
+  }
+  else if (currentChoice === 'scissors' && currentAnswer === 'rock'
+      || currentChoice === 'rock' && currentAnswer === 'paper'
+      || currentChoice === 'paper' && currentAnswer === 'scissors'){
     fill(0,255,0);
   }
   else{
@@ -168,24 +38,16 @@ function draw() {
   }
   text(currentAnswer, width/2,height/2);
 }
-  function mousePressed(){
-    currentAnimal = random(animals);
-    let reverseAnimal = reverseString(currentAnimal);
-    responsiveVoice.speak(reverseAnimal);
-  }
 
-  function guessAnimal(animal){
-    currentAnswer = animal.toLowerCase();
-    console.log(currentAnswer);
-  }
 
-  function reverseString(string) {
-    // Split the string into an array of characters
-    let characters = string.split('');
-    // Reverse the array of characters
-    let reverseCharacters = characters.reverse();
-    // Join the array of characters back into a string
-    let result = reverseCharacters.join('');
-    // Return the result
-    return result;
-  }
+function playRandom(choice) {
+  currentAnswer = choice.toLowerCase();
+  console.log(currentAnswer);
+
+  currentChoice = random(choices);
+  responsiveVoice.speak(currentChoice);
+
+  textSize(32);
+  textStyle(BOLD);
+  textAlign(CENTER,CENTER);
+}
