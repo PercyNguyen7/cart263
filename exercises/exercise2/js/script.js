4b6e4c012d67db3fe3d3dca4d27df18b2c332246
PercyNguyen7 <70938896+PercyNguyen7@users.noreply.github.com>
2021-02-04 14:48:19 -0500

    E2: Added in responses for robot in case user say anything else other than rock paper scissors. Also added in text to clarify player and robot's pick

--- exercises/exercise2/js/script.js
+++ exercises/exercise2/js/script.js
@@ -5,9 +5,16 @@ const choices = [
   "paper",
   "scissors",
     ];
+const cheatresponses = [
+  "Stop trying to cheat you lil bastard",
+  "You dare...cross me?",
+  "Are you ducking with me?",
+  "If you don't stop cheating I am going to crawl out of the sewer grate in your basement"
+];
 
 let currentChoice = ``;
 let currentAnswer = ``;
+let currentCheatResponse = ``;
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
@@ -24,30 +31,66 @@ function setup() {
 
 function draw() {
   background(0);
+  display();
+}
+
+function display(){
 
   if (currentChoice === currentAnswer){
-    fill(120);
+    text('TIE', width/2,height/2)
+    fill(140);
   }
   else if (currentChoice === 'scissors' && currentAnswer === 'rock'
       || currentChoice === 'rock' && currentAnswer === 'paper'
       || currentChoice === 'paper' && currentAnswer === 'scissors'){
+
+    text('You Won', width/2,height/2)
     fill(0,255,0);
   }
-  else{
+  else if (currentChoice === 'rock' && currentAnswer ==='scissors'
+  || currentChoice === 'paper' && currentAnswer === 'rock'
+  || currentChoice === 'scissors' && currentAnswer === 'paper') {
+    text('You Lost', width/2,height/2)
     fill(255,0,0);
+
   }
-  text(currentAnswer, width/2,height/2);
+  else {
+    text(currentCheatResponse, width/2,height/2)
+    text('Your Pick:', width/4, height/3);
+    text('Their Pick:', width/4, 2*height/3);
+    fill(0,0,255);
+  }
+  text('Your Pick:', width/4, height/3);
+  text('Their Pick:', width/4, 2*height/3);
+  text(currentAnswer,width/2,height/3)
+  text(currentChoice, width/2,2*height/3)
+
 }
-
-
 function playRandom(choice) {
-  currentAnswer = choice.toLowerCase();
-  console.log(currentAnswer);
-
-  currentChoice = random(choices);
-  responsiveVoice.speak(currentChoice);
-
   textSize(32);
   textStyle(BOLD);
   textAlign(CENTER,CENTER);
+  currentAnswer = choice.toLowerCase();
+  console.log(currentAnswer);
+
+
+
+  if (currentAnswer === 'rock' || currentAnswer ==='scissors' || currentAnswer === 'paper'){
+    currentChoice = random(choices);
+  responsiveVoice.speak(currentChoice,"US English Male",{
+    pitch:1,
+    rate:1,
+    volume:1,
+  });
+  }
+
+  else {
+  currentChoice =random(cheatresponses);
+  responsiveVoice.speak(currentChoice, "UK English Male",{
+    pitch:0.7,
+    rate:0.5,
+    volume:3,
+  });
+  }
+
 }
