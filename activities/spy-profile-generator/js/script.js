988d33483c0b793f3667673bdd086e8b13f3a633
PercyNguyen7 <70938896+PercyNguyen7@users.noreply.github.com>
2021-02-12 01:10:57 -0500

    E3: User must now say "The Password" to be granted access, then may log in with a name or old password if there is already data!

--- activities/spy-profile-generator/js/script.js
+++ activities/spy-profile-generator/js/script.js
@@ -1,4 +1,6 @@
 "use strict";
+let state = `menu`;
+
 let spyProfile = {
   name:`**REDACTED**`,
   species: `**REDACTED**`,
@@ -16,8 +18,6 @@ let objectData = undefined;
 let tarotData = undefined;
 let personalitytestData = undefined;
 
-
-
 function preload() {
   instrumentData = loadJSON(`https://raw.githubusercontent.com/dariusk/corpora/master/data/music/instruments.json`);
   monsterData = loadJSON(`https://raw.githubusercontent.com/dariusk/corpora/master/data/mythology/monsters.json`);
@@ -30,10 +30,22 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
+  if (annyang) {
+       let commands = {
+           'The Password': changestate
+       };
+
+       annyang.addCommands(commands);
+       annyang.start();
+     }
+  }
+
+function loadProfile(){
   let data = JSON.parse(localStorage.getItem(`spy-profile-data`));
+  state = 'infopage'
   if (data !== null){
-    // let password = prompt(`Agent! What is your password?!`);
-    // if (password === data.password){
+    let password = prompt(`Welcome back, Upwind unit! We're glad you stil' alive and kickin'. Enter your individual password to be granted access!`);
+    if (password === data.password){
     spyProfile.name = data.name;
     spyProfile.alias = data.alias;
     spyProfile.species = data.species;
@@ -41,6 +53,7 @@ function setup() {
     spyProfile.secretWeapon = data.secretWeapon;
     spyProfile.catchphrase = data.catchphrase;
     spyProfile.password = data.password;
+     }
     }
   else{
     generateSpyProfile();
@@ -59,8 +72,6 @@ function generateSpyProfile(){
   spyProfile.password = card.name;
 
 
-
-
   localStorage.setItem(`spy-profile-data`,JSON.stringify(spyProfile));
 }
 function setSpyProfile(){
@@ -68,7 +79,52 @@ function setSpyProfile(){
 }
 function draw() {
   background(0);
+  if (state ===`menu`){
+    menu()
+  }
+  else if (state === `access`){
+    access()
+  }
+  else if (state === `infopage`){
+    infopage()
+  }
+}
 
+// player must literally say "THE PASSWORD" to advance
+function menu(){
+  push();
+  background(0);
+  fill(210,0,0);
+  textFont(`Courier,monospace`);
+  textSize(70);
+  textAlign(CENTER,CENTER);
+  fill(210,0,0);
+  text(`Operation Upwind`,width/2, height/2-50);
+  textSize(60);
+  text(`SAY THE PASSWORD`,width/2,height/2+100);
+  pop();
+
+}
+
+function changestate(){
+  state =`access`;
+}
+function access(){
+  push();
+  background(0);
+  fill(210,0,0);
+  textFont(`Courier,monospace`);
+  textSize(50);
+  textAlign(CENTER,CENTER);
+  fill(210,0,0);
+  text(`Access to Operation Upwind Data Center Granted`,width/2,height/2 -50);
+  textSize(40);
+  text(`Press Enter to Log In`,width/2,height/2+50);
+  pop();
+}
+
+
+function infopage(){
   let profile = `** SPY PROFILE! DO NOT DISTRIBUTE! **
 Name: ${spyProfile.name}
 Alias:  ${spyProfile.alias}
@@ -85,13 +141,21 @@ Password:  ${spyProfile.password}`;
   textAlign(LEFT,TOP);
   fill(210,0,0);
   text(profile,100,100);
+  textSize(30);
+  textStyle(NORMAL);
+  text(`Press Space to delete your confidential data`, width/3, 4*height/5)
   pop();
 }
 
 function keyPressed(){
-  if (keyCode === ENTER) {
-
+  // if (state === `menu`&& keyCode === ENTER){
+  //   state =`infopage`
+  // }
+  if (state === `infopage` && keyCode === 32) {
            localStorage.removeItem(`spy-profile-data`);
            location.reload();
+  }
+  else if (state === `access` && keyCode === ENTER){
+    loadProfile();
   }
 }
