8a10c206a49e58ac5e8b2b43568971351b8b61d3
PercyNguyen7 <70938896+PercyNguyen7@users.noreply.github.com>
2021-02-20 00:37:28 -0500

    P1: Added in handpose and User's index finger now acts as a white wand

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -6,11 +6,18 @@ Percy Vinh TUan Dat Nguyen
 
 Description
 */
+//
+// First state is game menu
+let state = `loading`;
 
-
-let state = `menu`;
-
+// Word string for current spell being casted
 let currentSpell = ``;
+
+// predictions array
+let predictions = [];
+
+// User Webcam
+let video = undefined;
 
 /**
 Description of preload
@@ -24,6 +31,23 @@ Description of setup
 function setup() {
 createCanvas(640,480);
 
+  // access user's webcam
+  video = createCapture(VIDEO);
+  video.hide();
+
+  // load the handpose model
+  handpose = ml5.handpose(video, {
+    flipHorizontal: true
+  }, function(){
+    console.log(`Model loaded.`);
+    state = `menu`;
+  });
+
+  // Listen for predictions
+  handpose.on(`predict`,function(results){
+    console.log(results);
+    predictions = results;
+  });
 
   if (annyang) {
      let commands = {
@@ -56,26 +80,38 @@ function draw() {
 function menu(){
   background(255,200,200);
 
-  if (currentSpell === `Expelliarmus`){
-    ellipse(0,0,50);
-  }
+  if (predictions.length > 0){
+     let hand = predictions[0];
+
+  let index = hand.annotations.indexFinger;
+      let tip = index[3];
+      let base = index[0];
+      let tipX = tip[0];
+      let tipY = tip[1];
+      let baseX = base[0];
+      let baseY = base[1];
+      push();
+      noFill();
+      strokeWeight(3);
+      stroke(255);
+      line(baseX, baseY, tipX, tipY);
+      pop();
 
 
-  text(currentSpell + `!`, width / 3, height / 3)
+      if (currentSpell === `Expelliarmus`){
+        ellipse(0,0,50);
+      }
+    }
+    displaySpell()
 }
 
-
+function displaySpell(){
+  text(currentSpell + `!`, width / 2, height / 8)
+}
 function castSpell(spell){
   currentSpell = spell[0].toUpperCase() + spell.substring(1);
   console.log(currentSpell);
 
 
 
-
-
-
-  // if (currentSpell = `hello`) {
-  //   fill(0);
-  //   ellipse(100,100,50);
-  // }
 }
