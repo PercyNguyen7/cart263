392e8807cb2e1aa3e65436f03da424887ac5723e
PercyNguyen7 <70938896+PercyNguyen7@users.noreply.github.com>
2021-04-23 01:53:53 -0400

    P2: Started P2 with code from the Proposal

---
+++ projects/project2/js/Phone.js
@@ -0,0 +1,27 @@
+class Phone {
+  constructor() {
+    this.x = width / 2,
+    this.y = height / 2 - 300,
+    this.width = 120,
+    this.height = 240,
+    this.vy = 7
+
+  }
+  // Display black rectangle. Slightly transparent if player casts Lumos and completely transparent if player casts Lumos Maxima.
+  display() {
+    push();
+    fill(20);
+    rect(this.x, this.y, this.width,this.height);
+    fill(0,150,240);
+    rect(this.x, this.y+5, this.width - 20, this.height - 30);
+    pop();
+  }
+  // Move
+  move(){
+    this.y += this.vy
+    if (this.y -this.height/2 >= height){
+      state =`phoneFallenEnding`
+    }
+  }
+
+  }
